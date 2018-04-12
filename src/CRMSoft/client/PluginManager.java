f20bdd2176f108a2bfbb9b311bafab3798d2cb35
Christoph Cudly <christoph.cudly@gmail.com>
2018-04-12 08:42:52 +0200

    Classloader Hotfix 2

--- src/CRMSoft/client/PluginManager.java
+++ src/CRMSoft/client/PluginManager.java
@@ -1,20 +1,24 @@
 package CRMSoft.client;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import PluginInterface.client.PluginInterface;
 
 public class PluginManager {
 
 	public static HashMap<String, PluginInterface> loadedPlugins = new HashMap<>();
-	
+
 	public static HashMap<String, PluginInterface> loadPlugins() {
 
 		File pluginPath = new File("plugins");
@@ -29,15 +33,32 @@ public class PluginManager {
 				Object URI;
 
 				try {
-					URL[] urls = { new URL("jar:file:" + pluginPath.getPath()+"!/") };
+					URL[] urls = { new URL("jar:file:" + pluginPath.getPath() + "/" + plugin.getName() + "!/") };
+					System.out.println(urls[0]);
 					URLClassLoader cLoder = URLClassLoader.newInstance(urls);
-					Class classToLoad = Class.forName(plugin.getName().split("\\.")[0] + ".Plugin", true, cLoder);
-					Constructor<?> constructor = classToLoad.getConstructor();
-					PluginInterface pluginInstance = (PluginInterface) constructor.newInstance();
 
-					System.out.println(pluginInstance.getName() + " loaded");
+					JarFile jarFile = new JarFile(pluginPath.getPath() + "/" + plugin.getName());
+					Enumeration<JarEntry> e = jarFile.entries();
 
-					loadedPlugins.put(pluginInstance.getName(), pluginInstance);
+					while (e.hasMoreElements()) {
+						JarEntry je = e.nextElement();
+						if (je.isDirectory() || !je.getName().endsWith(".class")) {
+							continue;
+						}
+						// -6 because of .class
+						String className = je.getName().substring(0, je.getName().length() - 6);
+						className = className.replace('/', '.');
+						if (className.equals(plugin.getName().split("\\.")[0] + ".Plugin")) {
+							Class classToLoad = cLoder.loadClass(className);
+							Constructor<?> constructor = classToLoad.getConstructor();
+							PluginInterface pluginInstance = (PluginInterface) constructor.newInstance();
+
+							System.out.println(pluginInstance.getName() + " loaded");
+
+							loadedPlugins.put(pluginInstance.getName(), pluginInstance);
+						}
+
+					}
 
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
@@ -63,6 +84,9 @@ public class PluginManager {
 				} catch (MalformedURLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
 			}
 		}
